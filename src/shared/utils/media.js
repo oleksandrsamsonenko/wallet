baf10f209133a894556817dba0ea97939358d48b
YevheniiKravchukUA <Kravchukmail23@gmail.com>
2023-03-15 14:24:22 +0100

    add media variables to shared/utils/media.js

--- src/shared/utils/media.js
+++ src/shared/utils/media.js
@@ -1,1 +1,20 @@
-// import { useMediaQuery } from 'react-responsive';
+export const mobileMax = { query: '(max-width: 767px)' };
+export const mobileResp = { query: '(max-width: 573px)' };
+
+export const tabletMin = { query: '(max-width: 768px)' };
+export const tabletMax = { query: '(max-width: 1279px)' };
+
+export const desktopMin = { query: '(max-width: 1280px)' };
+
+export const retina = { query: '(min-resolution: 2dppx)' };
+
+// import { useMediaQuery } from 'react-responsive'
+// import * as media from "../path-here"
+
+// const isDesktop = useMediaQuery(media.desktopMin)
+
+// MyComponent () {
+// return (
+//      {isDesktop && <div>Покажет див только при медиа > 1280px</div>}
+// )
+// }
