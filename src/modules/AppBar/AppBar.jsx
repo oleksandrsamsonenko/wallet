f7c4104972bdd5df435feafb15d161eadbfa4e70
Yevhenii Kravchuk <115158804+YevheniiKravchukUA@users.noreply.github.com>
2023-03-16 09:57:06 +0100

    Update AppBar.jsx

--- src/modules/AppBar/AppBar.jsx
+++ src/modules/AppBar/AppBar.jsx
@@ -6,11 +6,11 @@ import NavBarMobile from './NavBarMobile/NavBarMobile';
 import styles from './AppBar.module.scss';
 
 const AppBar = () => {
-  const isDesktop = useMediaQuery({ minWidth: 1280 });
+//   const isDesktop = useMediaQuery({ minWidth: 1280 });
   const isTabletMin = useMediaQuery({ minWidth: 768 });
   const isTabletMax = useMediaQuery({ maxWidth: 767 });
-  const isMobileMin = useMediaQuery({ maxWidth: 480 });
-  const isMobileMax = useMediaQuery({ maxWidth: 767 });
+//   const isMobileMin = useMediaQuery({ maxWidth: 480 });
+//   const isMobileMax = useMediaQuery({ maxWidth: 767 });
 
   return (
     <div className={styles.AppBar}>
