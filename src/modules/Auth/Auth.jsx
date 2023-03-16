95b6a62e1ec75d3d676ffc9e50de8398c14a3454
Anka <103377387+ann153476@users.noreply.github.com>
2023-03-16 17:07:58 +0200

    bacground image boy and girl

--- src/modules/Auth/Auth.jsx
+++ src/modules/Auth/Auth.jsx
@@ -1,14 +1,10 @@
 import { Link, Outlet } from 'react-router-dom';
 import css from './Auth.module.scss';
-import logo from '../../assets/svg/main-logo.svg';
+
 const Auth = () => {
   return (
-    <div className={css.auth}>
-      <Link to="/" className={css.logo}>
-        <img src={logo} alt="logo" />
-        <h2 className={css.title}> Wallet</h2>
-      </Link>
-
+    <div className={`auth__box ${css.auth}`}>
+      <Link to="/" className={css.logo} />
       <Outlet />
     </div>
   );
