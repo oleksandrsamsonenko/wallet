224090bdea8b08d87b19030a5bc2f7c7fb98c1d4
AntonBabenko96 <ababenko159@gmail.com>
2023-03-18 10:15:06 +0200

    commit
    добавив сторінку коли немає транзакцій

--- src/pages/TransactionList/TransactionListMobile.jsx
+++ src/pages/TransactionList/TransactionListMobile.jsx
@@ -6,6 +6,7 @@ import {
 } from 'redux/AddTransaction/addTransaction-operations';
 import css from '../TransactionList/TransactionListMobile.module.scss';
 import svg from '../../assets/svg/edit-02.svg';
+import notfound from '../../assets/background/notfound.png';
 
 const TransactionListMobile = () => {
   const dispatch = useDispatch();
@@ -15,72 +16,107 @@ const TransactionListMobile = () => {
   }, [dispatch]);
   const categories = useSelector(state => state.categories.categories);
   const transactions = useSelector(state => state.categories.history);
-  return transactions?.map(
-    ({ id, transactionDate, type, comment, amount, categoryId }) => {
-      const date = new Date(transactionDate).toLocaleDateString();
-      const transactionName = categories.find(
-        category => category.id === categoryId
-      );
-      return (
-        <div className={css.box}>
-          <table className={css.table} key={id}>
-            <tbody className={css.tbody}>
-              <tr className={css.part}>
-                <td className={css.pointFirst}></td>
-                <td className={css.head}>Date</td>
-                <td className={css.body}>{date}</td>
-              </tr>
-              <tr>
-                <td className={css.point}></td>
-                <td className={css.head}>Type</td>
-                {type === 'EXPENSE' ? (
-                  <td className={css.body}>-</td>
-                ) : (
-                  <td className={css.body}>+</td>
-                )}
-              </tr>
-              <tr>
-                <td className={css.point}></td>
-                <td className={css.head}>Category</td>
-                <td className={css.body}>{transactionName?.name}</td>
-              </tr>
-              <tr>
-                <td className={css.point}></td>
-                <td className={css.head}>Comment</td>
-                <td className={css.body}>{comment}</td>
-              </tr>
-              <tr>
-                <td className={css.point}></td>
-                <td className={css.head}>Sum</td>
-                {type === 'EXPENSE' ? (
-                  <td className={css.expence}>{Math.abs(amount).toFixed(2)}</td>
-                ) : (
-                  <td className={css.income}>{amount.toFixed(2)}</td>
-                )}
-              </tr>
-              <tr>
-                <td className={css.pointLast}></td>
-                <td className={css.head}>
-                  <button
-                    className={css.btn}
-                    type="button"
-                    onClick={() => dispatch(deleteTransactions(id))}
-                  >
-                    Delete
-                  </button>
-                </td>
-                <td className={css.bodyLink}>
-                  <button className={css.editBtn}>
-                    <img src={svg} alt="Edit" className={css.svg} />
-                    <span> Edit</span>
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      );
-    }
-  );
+  if (transactions.length !== 0) {
+    return transactions?.map(
+      ({ id, transactionDate, type, comment, amount, categoryId }) => {
+        const date = new Date(transactionDate).toLocaleDateString();
+        const transactionName = categories.find(
+          category => category.id === categoryId
+        );
+        return (
+          <div className={css.box}>
+            <table className={css.table} key={id}>
+              <tbody className={css.tbody}>
+                <tr className={css.part}>
+                  {type === 'EXPENSE' ? (
+                    <td className={css.pointFirstExpence}></td>
+                  ) : (
+                    <td className={css.pointFirstIncome}></td>
+                  )}
+                  <td className={css.head}>Date</td>
+                  <td className={css.body}>{date}</td>
+                </tr>
+                <tr>
+                  {type === 'EXPENSE' ? (
+                    <td className={css.pointExpence}></td>
+                  ) : (
+                    <td className={css.pointIncome}></td>
+                  )}
+                  <td className={css.head}>Type</td>
+                  {type === 'EXPENSE' ? (
+                    <td className={css.body}>-</td>
+                  ) : (
+                    <td className={css.body}>+</td>
+                  )}
+                </tr>
+                <tr>
+                  {type === 'EXPENSE' ? (
+                    <td className={css.pointExpence}></td>
+                  ) : (
+                    <td className={css.pointIncome}></td>
+                  )}
+                  <td className={css.head}>Category</td>
+                  <td className={css.body}>{transactionName?.name}</td>
+                </tr>
+                <tr>
+                  {type === 'EXPENSE' ? (
+                    <td className={css.pointExpence}></td>
+                  ) : (
+                    <td className={css.pointIncome}></td>
+                  )}
+                  <td className={css.head}>Comment</td>
+                  <td className={css.body}>{comment}</td>
+                </tr>
+                <tr>
+                  {type === 'EXPENSE' ? (
+                    <td className={css.pointExpence}></td>
+                  ) : (
+                    <td className={css.pointIncome}></td>
+                  )}
+                  <td className={css.head}>Sum</td>
+                  {type === 'EXPENSE' ? (
+                    <td className={css.expence}>
+                      {Math.abs(amount).toFixed(2)}
+                    </td>
+                  ) : (
+                    <td className={css.income}>{amount.toFixed(2)}</td>
+                  )}
+                </tr>
+                <tr>
+                  {type === 'EXPENSE' ? (
+                    <td className={css.pointLastExpence}></td>
+                  ) : (
+                    <td className={css.pointLastIncome}></td>
+                  )}
+                  <td className={css.head}>
+                    <button
+                      className={css.btn}
+                      type="button"
+                      onClick={() => dispatch(deleteTransactions(id))}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                  <td className={css.bodyLink}>
+                    <button className={css.editBtn}>
+                      <img src={svg} alt="Edit" className={css.svg} />
+                      <span> Edit</span>
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        );
+      }
+    );
+  } else {
+    return (
+      <div className={css.boxNotFound}>
+        <p className={css.text}>There have been no transactions added yet.</p>
+        <img src={notfound} alt="no transaction" className={css.img} />
+      </div>
+    );
+  }
 };
 export default TransactionListMobile;
