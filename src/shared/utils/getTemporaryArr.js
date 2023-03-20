6ef89d25cad66425ddfbf9a55fcec896c61c231f
Burmikk <byrmik@gmail.com>
2023-03-20 04:14:25 +0200

    add logic to filter by date

---
+++ src/shared/utils/getTemporaryArr.js
@@ -0,0 +1,27 @@
+//Первая функция которая получает историю транзакций и модернизирует для дальнейшего использования
+const getTemporaryArr = (history, categories) => {
+  //Фильтрует history чтоб убрать "INCOME"
+  const filteredExpenses = history.filter(hist => hist.type === 'EXPENSE');
+
+  //Добавляет в history необходимые поля, а именно color и name
+  const temporaryArr = filteredExpenses.map(expenses => {
+    return {
+      ...expenses,
+      color: categories.reduce((acc, item) => {
+        if (item.id === expenses.categoryId) {
+          return (acc = item.color);
+        }
+        return acc;
+      }, ''),
+      name: categories.reduce((acc, item) => {
+        if (item.id === expenses.categoryId) {
+          return (acc = item.name);
+        }
+        return acc;
+      }, ''),
+    };
+  });
+  return temporaryArr;
+};
+
+export default getTemporaryArr;
